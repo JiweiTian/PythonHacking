a7316b13dcf6500bfb4cd72002a5086bfbe1cad8
JiweiTian <45908316+JiweiTian@users.noreply.github.com>
2018-12-23 11:10:22 +0800

    Add files via upload

---
+++ 1.4 流程控制/login.py
@@ -0,0 +1,7 @@
+user=str(input('请输入用户名：'))
+password=str(input('请输入密码：'))
+
+if user=='seven'and password=='123':
+    print("login suceess")
+else:
+    print("login failed")
---
+++ 1.4 流程控制/whileadd.py
@@ -0,0 +1,12 @@
+sum=0
+n=2
+while n<=100:
+    if n%2==0:
+        sum=sum+n
+    else:
+        sum=sum-n
+    n+=1
+
+print(sum)
+
+
